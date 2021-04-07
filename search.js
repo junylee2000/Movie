ae55991e52ed5115a5e9fe8c03344a949580f579
junylee2000 <66085324+junylee2000@users.noreply.github.com>
2021-04-07 13:29:49 -0700

    Update search.js

--- search.js
+++ search.js
@@ -1,1 +1,17 @@
-
+//search javascript
+function searchMedia() {
+  var input, filter, ol, li, a , i, txtValue;
+  input = document.getElementById('searchBar');
+  filter = input.value.toUpperCase();
+  ol = document.getElementById("list");
+  li = ol.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++){
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue= a.textContent || a.innerText;
+    if(txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
